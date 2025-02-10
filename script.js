608926363f97ede356edae0de475d61a3c9f9827
kln001 <klnstudios.sc@gmail.com>
2025-02-11 00:18:30 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".btn").addEventListener("click", () => {
+        alert("Redirecting to demo request form...");
+    });
+});
